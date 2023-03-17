40f8669410aebd35b7df233d978a1a03fd0ab83b
Eshkar <100770281+eshkar21@users.noreply.github.com>
2023-03-17 07:41:18 +0200

    Update app.py

--- HTMLFramework/app.py
+++ HTMLFramework/app.py
@@ -1,17 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+#Main Screen
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#Student and Staff Registration
+@app.route('/form_reg')
+def regis():
+    return render_template('SSReg.html') 
+
+#Student and Staff Login
 @app.route('/new')
 def new_user():
-    return render_template("mainLogin.html")
+    return render_template("SSLogin.html")
+database={'22137553@dut4life.ac.za':'1234', '22113344': '12345'}
 
-@app.route('/profile')
+#Student and Staff Testing Login
+@app.route('/form_login',methods=['POST','GET'])
 def profile():
-    return render_template('mainReg.html')
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+        return render_template('SSLogin.html', info='Invalid User')
+    else:
+        if database[name1]!=pwd:
+            return render_template('SSLogin.html', info='Invalid Password')
+        else:
+            return render_template('SSMenu.html')
+
+
+
+#Maintenance Login
+#@app.route('/new')
+#def new_user():
+#    return render_template("mainLogin.html")
+#database={'bmw@bmw.com':'1234', 'audi@audi.com': 12345}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
